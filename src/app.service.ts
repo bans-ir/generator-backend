8bc50cbbe9e7a2bc01042f8364078ccaa3a6213f
mohsen noury <66866297+MNGH-27@users.noreply.github.com>
2025-06-27 22:32:58 +0330

    fix: fix send message

--- src/app.service.ts
+++ src/app.service.ts
@@ -16,6 +16,19 @@ export class AppService {
 
   async savePhoneNumber(phoneNumber: string) {
     const phone = this.phoneRepository.create({ phoneNumber });
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const Kavenegar = require('kavenegar');
+    const api = Kavenegar.KavenegarApi({
+      apikey:
+        '626173704752655247476B745969417235354E7942427A764839513561504139644B786B364C495A65446F3D',
+    });
+    api.Send({
+      message:
+        'ما اطلاعات مربوط به مصرف برق شما را دریافت کردیم برای دریافت اطلاعات بیشتر و راهنمایی های ما با ما در ارتباط باشید',
+      sender: '2000660110',
+      receptor: phoneNumber,
+    });
+
     return await this.phoneRepository.save(phone);
   }
 }
