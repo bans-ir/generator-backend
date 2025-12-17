0b9782f185613f943562cdd4533083582a21a481
Ali khandi <alikhandi@gmail.com>
2025-12-18 02:14:55 +0330

    add fucking cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors();
+app.enableCors({
+  origin: 'https://power.adadsoft.ir', // your frontend
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'User-Agent', 'sec-ch-ua', 'sec-ch-ua-platform', 'sec-ch-ua-mobile'],
+  credentials: true, // only if you use cookies or auth
+});
+
 
   app.useStaticAssets(path.join(__dirname, '..', 'public'), {
     prefix: '/public/',
