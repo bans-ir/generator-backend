a0dd1c85fed75e2980671e3b0344bd3a4948e03f
Ali khandi <alikhandi@gmail.com>
2025-12-18 02:23:13 +0330

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-app.enableCors({
-  origin: 'https://power.adadsoft.ir', // your frontend
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'User-Agent', 'sec-ch-ua', 'sec-ch-ua-platform', 'sec-ch-ua-mobile'],
-  credentials: true, // only if you use cookies or auth
-});
+// app.enableCors({
+//   origin: 'https://power.adadsoft.ir', // your frontend
+//   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'User-Agent', 'sec-ch-ua', 'sec-ch-ua-platform', 'sec-ch-ua-mobile'],
+//   credentials: true, // only if you use cookies or auth
+// });
 
 
   app.useStaticAssets(path.join(__dirname, '..', 'public'), {
